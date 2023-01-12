acea1043f7ae470ea123af2c9ce5ede6660c2971
Lebedev Vadim <v.lebedev@qsoft.ru>
2023-01-12 16:41:05 +0800

    9 / Add grades and positions methods

--- src/Contracts/Client.php
+++ src/Contracts/Client.php
@@ -6,6 +6,8 @@ use Awesome\Connector\Contracts\Request;
 
 interface Client
 {
-    public function employees(): Request;
-    public function vacations(): Request;
+    public function employees(bool $activeOnly = true): Request;
+    public function grades(array $ids = []): Request;
+    public function positions(array $ids = []): Request;
+    public function vacations(bool $activeOnlyEmployees = true): Request;
 }
